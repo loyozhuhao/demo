35b3aaebf301af430defdb4446c9607d12d67903
zhuhao <642702011@qq.com>
2019-11-04 21:57:10 +0800

    demo

---
+++ goroutine/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func main() {
+num:=runtime.NumCPU()
+fmt.Println(num)
+runtime.GOMAXPROCS(1)
+	go say("world")
+	 say("hello")
+
+	fmt.Println("NumGoroutine",runtime.NumGoroutine())
+}
